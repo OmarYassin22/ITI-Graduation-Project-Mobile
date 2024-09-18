ccfc4af07626bde7f3cf96bfea22d15578380d2f
omar yassin <o.y.sleem@gmail.com>
2024-09-18 17:01:10 +0300

    Initial Names

---
+++ Common/ScreenNames.js
@@ -0,0 +1,13 @@
+const ScreenNames = {
+  Home: "Home",
+  About: "About",
+  Profile: "Profile",
+  Settings: "Settings",
+  Login: "Login",
+  Contact: "Contact",
+  Register: "Register",
+  Courses: "Courses",
+  ForgotPassword: "ForgotPassword",
+};
+
+export default ScreenNames;
