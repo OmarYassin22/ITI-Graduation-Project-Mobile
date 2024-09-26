c21b03622b3ffc63515735bab08a15ce2f238726
Emad Elshplangy <elshplngy.emad@gmail.com>
2024-09-26 18:20:18 +0300

    add instructor

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { Platform, View } from 'react-native';
 import CourseCards from './ScreenComponts/Courses';
+import AddVideo from './ScreenComponts/instructor/addvideo';
+import Table from './ScreenComponts/instructor/table';
 
 export default function App() {
   return (
-    <CourseCards />
+    // <CourseCards />
+    <>
+      <AddVideo/>
+      {/* <Table/> */}
+    </>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,37 @@
+// firebaseConfig.js
+import { initializeApp, getApps } from "firebase/app";
+import { initializeAuth, getReactNativePersistence, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeUXPKzKQ-Ij8oMtzqI9C_cWUrbPo2GQE",
+  authDomain: "grad-projcet.firebaseapp.com",
+  projectId: "grad-projcet",
+  storageBucket: "grad-projcet.appspot.com",
+  messagingSenderId: "880621406110",
+  appId: "1:880621406110:web:5be96f9fe4fcfb0a8af88f",
+};
+
+let app;
+let auth;
+let db;
+let storage;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  });
+  db = getFirestore(app);
+  storage = getStorage(app);
+} else {
+  app = getApps()[0];
+  auth = getAuth(app);
+  db = getFirestore(app);
+  storage = getStorage(app);
+}
+
+export { app, db, auth, storage };
